d088dfc4400b4156589ab7a2ea1302e3e454d3c6
Alpay Kuzu <201805057@stu.adu.edu.tr>
2023-10-23 02:12:36 +0300

    Update
    Movement

---
+++ Castle2D/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float maxSpeed, minSpeed, jumpSpeed;
+    private float tempSpeed;
+    public float jumpPower;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    public GameObject jumpPoint;
+    float jumpTimer = 0f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        tempSpeed = maxSpeed;
+    }
+    private void Update()
+    {
+        Movement();
+        Jump();
+        isGrounded = jumpPoint.GetComponent<JumpPoint>().isGrounded;
+    }
+
+    void Movement()
+    {
+        // sağa sola hareket ve dönme
+        Vector2 control = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            maxSpeed = Mathf.Lerp(minSpeed, maxSpeed, Mathf.Abs(Input.GetAxis("Horizontal")));
+            transform.rotation = Quaternion.Euler(0, -180,0);
+            transform.Translate(control * maxSpeed * Time.deltaTime * -1); 
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            maxSpeed = Mathf.Lerp(minSpeed, maxSpeed, Mathf.Abs(Input.GetAxis("Horizontal")));
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            transform.Translate(control * maxSpeed * Time.deltaTime * 1);
+        }
+        else
+        {
+            maxSpeed = tempSpeed;
+        }
+    }
+    void Jump()
+    {
+        //zıplama
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpPower /10 , ForceMode2D.Impulse);
+            isGrounded = false;
+            
+        }
+        if(!isGrounded)
+        {
+            maxSpeed = jumpSpeed;
+            jumpTimer += Time.deltaTime;
+        }
+        else
+        {
+            maxSpeed = tempSpeed;
+            jumpTimer = 0;
+        }
+        // gravity
+
+        if (jumpTimer > 0.2f)
+        {
+            rb.gravityScale = jumpTimer*4;
+        }
+        else
+        {
+            rb.gravityScale = 1.0f;
+        }
+        if(rb.gravityScale>5f)
+        {
+            rb.gravityScale = 5.1f;
+        }
+    }
+
+}
---
+++ Castle2D/Assets/Scripts/JumpPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPoint : MonoBehaviour
+{
+    public bool isGrounded;
+
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
